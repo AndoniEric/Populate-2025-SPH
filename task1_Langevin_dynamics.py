8522db8a95fd73d618f611476041c93d6d587479
Eric Andoni <108674878+AndoniEric@users.noreply.github.com>
2025-06-20 11:06:21 +0200

    Created file for task 1
    It only contains comments

---
+++ task1_Langevin_dynamics.py
@@ -0,0 +1,4 @@
+# Code for: LANGEVIN DYNAMICS SIMULATION: CHAINS OF MAGNETIC PARTICLES IN A FLUID UNDER ROTATING MAGNETIC FIELDS
+#In the context of Populate 2025
+#By Anna Macaluso, Tran Thi Quynh Anh and Eric Andoni
+#Guided by Adolfo Vazquez Quesada & Christophe Henry
